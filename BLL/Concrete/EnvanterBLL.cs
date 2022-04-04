c32270260f2dca75153ca04e001905fe3a6f4f03
Erkin Akdeniz <diastechccn@gmail.com>
2022-04-04 14:43:44 +0300

    for dongusu kaldirildi

--- BLL/Concrete/EnvanterBLL.cs
+++ BLL/Concrete/EnvanterBLL.cs
@@ -46,10 +46,7 @@ namespace BLL.Concrete
 
         public List<Envanter> Search(string arananKelime)
         {
-            for (int i = 0; i < int.MaxValue; i++)
-            {
-                return _envanterRepository.GetEx(x => x.Ad.ToUpper().Contains(arananKelime.ToUpper())).ToList();
-            }
+            
             return _envanterRepository.GetEx(x => x.Ad.ToUpper().Contains(arananKelime.ToUpper())).ToList();
         }
 
